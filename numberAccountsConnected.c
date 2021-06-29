98a811a7820403d91d4b7c3596214ef6a87da591
ZAWARTO <62563083+ZAWARTO@users.noreply.github.com>
2021-06-29 18:17:17 +0200

    Update numberAccountsConnected.c

--- numberAccountsConnected.c
+++ numberAccountsConnected.c
@@ -16,7 +16,8 @@
 #include <fcntl.h>
 #define error(a) {perror(a); exit(1);};
 
-int main(int argc, char *argv[]){ //cuantas cuentas ISO acaf00xx conectadas
+int main(int argc, char *argv[])
+{ 
 
    int pfd1[2], pfd2[2], pid;
 
@@ -24,11 +25,10 @@ int main(int argc, char *argv[]){ //cuantas cuentas ISO acaf00xx conectadas
 
    printf("parent: pipe created, channels: READ=%d and WRITE=%d\n", pfd1[0], pfd1[1]);
 
-   switch(pid = fork()){
-
+   switch(pid = fork())
+   {
       case -1: error("fork");
       case 0: /*1st child: who*/
-
          printf("1st child process created\n");
          if (close(1)==-1) error ("close");
          if (dup(pfd1[1]) != 1) error("dup");
@@ -36,36 +36,37 @@ int main(int argc, char *argv[]){ //cuantas cuentas ISO acaf00xx conectadas
          execlp("who", "who", NULL);
          error("who execlp");
  }
+   
    printf("parent: who(%d) process launched\n", pid);
    close(pfd1[1]);
 
    if(pipe(pfd2) != 0) error("pipe");
 
    printf("parent: pipe created, channels: READ=%d and WRITE=%d\n", pfd2[0], pfd2[1]);
-   switch(pid = fork()){
-
+   
+   switch(pid = fork())
+   {
       case -1: error("fork");
       case 0: /*2nd child: grep acaf00*/
-
          printf("1st child process created\n");
          close(pfd2[0]);
          if (close(0)==-1) error ("close");
          if (dup(pfd1[0]) != 0) error("dup");
          close(pfd1[0]);
-
          if (close(1)==-1) error ("close");
          if (dup(pfd2[1]) !=1) error("dup");
          close(pfd2[1]);
          execlp("grep", "grep", argv[1], NULL);
          error("grep execlp");
     }
+   
    printf("parent: grep(%d) process launched\n", pid);
 
    close(pfd1[0]);
    close(pfd2[1]);
 
-   switch(pid = fork()){
-
+   switch(pid = fork())
+   {
       case -1: error("fork");
       case 0: /*3rd child: wc -l*/
          if (close(0)==-1) error ("close");
@@ -74,9 +75,11 @@ int main(int argc, char *argv[]){ //cuantas cuentas ISO acaf00xx conectadas
          execlp("wc", "wc", "-l", NULL);
          error("wc execlp");
    }
+   
    printf("parent: wc(%d) process launched\n", pid);
    close(pfd2[0]);
-   while ((pid = wait(NULL)) != -1){
+   while ((pid = wait(NULL)) != -1)
+   {
       printf("parent: %d process finished\n", pid);
    }
    return 0;
